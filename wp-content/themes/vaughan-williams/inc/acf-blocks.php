5ead1fc80c183ed6a4f8dfc9087c5431b50f0671
Greig Cooke <greigcooke75@gmail.com>
2022-08-03 16:16:33 +0100

    publishers cpt and acf fields

--- wp-content/themes/vaughan-williams/inc/acf-blocks.php
+++ wp-content/themes/vaughan-williams/inc/acf-blocks.php
@@ -3,7 +3,7 @@
 add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
 function prefix_disable_gutenberg($current_status, $post_type)
 {
-    $post_types = ['letter', 'who-we-are'];
+    $post_types = ['letter', 'who-we-are', 'publisher'];
     if (in_array($post_type, $post_types)) return false;
     return $current_status;
 }
--- wp-content/themes/vaughan-williams/inc/template-posttypes.php
+++ wp-content/themes/vaughan-williams/inc/template-posttypes.php
@@ -24,7 +24,7 @@ function register_posttypes() {
 		'publicly_queryable' => true,
 		'show_ui' => true,
 		'query_var' => true,
-		'menu_icon' => 'dashicons-book',
+		'menu_icon' => 'dashicons-media-document',
 		'capability_type' => 'post',
 		'hierarchical' => false,
 		'menu_position' => 20,
@@ -63,6 +63,37 @@ function register_posttypes() {
 	);
 
 	register_post_type( 'who-we-are' , $args );
+
+	$labels = array(
+		'name' => _x('Publishers', 'post type general name'),
+		'singular_name' => _x('Publisher', 'post type singular name'),
+		'add_new' => _x('Add New', 'Publisher item'),
+		'add_new_item' => __('Add New Publisher item'),
+		'edit_item' => __('Edit Publisher item'),
+		'new_item' => __('New Publisher item'),
+		'view_item' => __('View Publisher item'),
+		'search_items' => __('Search Publishers'),
+		'not_found' =>  __('Nothing found'),
+		'not_found_in_trash' => __('Nothing found in Trash'),
+		'parent_item_colon' => ''
+	);
+
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'show_in_rest' => true,
+		'has_archive' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'query_var' => true,
+		'menu_icon' => 'dashicons-star-empty',
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'menu_position' => 20,
+		'supports' => array('title','author', 'editor', 'excerpt', 'thumbnail', 'page-attributes', 'custom-fields')
+	);
+
+	register_post_type( 'publisher' , $args );
 }
 
 add_action('init', 'register_posttypes');
